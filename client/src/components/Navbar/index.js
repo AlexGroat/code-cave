6b40b76fd4169695c78300e584c49d909046e916
Alex Groat <alex.groat.95@gmail.com>
2022-02-11 21:26:02 +1030

    deleting unused src folder

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
 
-const NavBar = () => {
-  // const logout = (event) => {
-  //   event.preventDefault();
-  //   Auth.logout();
-  // };
+// const NavBar = () => {
+//   // const logout = (event) => {
+//   //   event.preventDefault();
+//   //   Auth.logout();
+//   // };
 
-}
+// }
 
 
 export default Navbar;
