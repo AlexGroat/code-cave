31ec0e93dafb8fe2bdc3a6a2bb5c4ee177a87dc8
Alex Groat <alex.groat.95@gmail.com>
2022-02-14 08:21:51 +1030

    responsive issue

--- client/src/components/PostCard/PostCard.js
+++ client/src/components/PostCard/PostCard.js
@@ -1,26 +1,16 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
 
-function PostCard({
-    post: { body, username, createdAt, id, likes, likeCount, commentCount }
-}); {
-
-    return (
-        <div className="card-container">
-            <div className="card-body">
-            <div className="card-title">
-             <h2 className="card-user">
-                {username}
-             </h2>
-             </div>
-             <div className="card-text">
-                <p className="blog-text">
-                    {body}
-                </p>
-             </div>
-             </div>
-        </div>
-    )
-}
+const PostCard = ({ post }) => {
+  return (
+    <Card className="post-container">
+      <div className="post-body">
+        <h1 className="user-name">{post.username}</h1>
+        <p className="post-text">{post.body}</p>
+      </div>
+    </Card>
+  );
+};
 
 export default PostCard;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,14 +1,23 @@
 import PostForm from "../../components/PostForm/PostForm";
+import PostCard from "../../components/PostCard/PostCard";
+
 import "./home.css";
 
 export default function Home() {
   return (
-    
-    <>
-    
-      <div className="home-container">
+ <>
+ <div className="home-container">
+        <div className="col1 col-3" style={{backgroundColor: "lightblue"}}>
       <PostForm />
       </div>
-    </>
+        <div className="col2 col-5" style={{backgroundColor: "red"}}>
+      middle row for feed
+      </div>
+        <div className="col3 col-4"style={{backgroundColor: "green"}}>
+      right row for news api
+      </div>
+      </div>
+ </>
+
   );
 }
