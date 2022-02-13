3468d62725352311ecdc4a605d1b97f583296776
Alex Groat <alex.groat.95@gmail.com>
2022-02-14 08:26:16 +1030

    update

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -7,13 +7,13 @@ export default function Home() {
   return (
  <>
  <div className="home-container">
-        <div className="col1 col-3" style={{backgroundColor: "lightblue"}}>
+        <div className="column-1 col-3" style={{backgroundColor: "lightblue"}}>
       <PostForm />
       </div>
-        <div className="col2 col-5" style={{backgroundColor: "red"}}>
+        <div className="column-2 col-5" style={{backgroundColor: "red"}}>
       middle row for feed
       </div>
-        <div className="col3 col-4"style={{backgroundColor: "green"}}>
+        <div className="column-3 col-4"style={{backgroundColor: "green"}}>
       right row for news api
       </div>
       </div>
