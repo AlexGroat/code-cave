998c27ea86b3f0cf64fa87d906ae3579298aacab
Alex Groat <alex.groat.95@gmail.com>
2022-02-13 13:38:25 +1030

    finished top bar

--- client/src/components/Topbar/TopBar.js
+++ client/src/components/Topbar/TopBar.js
@@ -1,20 +1,33 @@
 import "./topBar.css";
+import CodeIcon from "@material-ui/icons/Code";
+import { Search } from "@material-ui/icons";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
 export default function TopBar() {
   return (
     <div className="topbar-container">
       <div className="topbar-left">
-          <span className="cc-header">Code Cave</span>
+        <span className="cc-header">
+          <CodeIcon />
+          Code Cave
+        </span>
       </div>
       <div className="topbar-center">
-          <div className="cc-slogan">
-              The cave for all your coding solutions!
-          </div>
+        <div className="cc-slogan">
+          <ArrowBackIosIcon />
+          The cave for all your coding solutions!
+          <ArrowForwardIosIcon />
+        </div>
       </div>
       <div className="topbar-right">
-          <div className="cc-end">
-              hello
-          </div>
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for a user or post"
+            className="searchInput"
+          />
+        </div>
       </div>
     </div>
   );
