121a8980d8a2c028c0dafb2b6c81a90fa889ea4f
Alex Groat <alex.groat.95@gmail.com>
2022-02-15 18:53:03 +1030

    working on single post page

---
+++ client/src/pages/SinglePost/SinglePost.js
@@ -0,0 +1,13 @@
+import PostForm from "../../components/PostForm/PostForm";
+import PostCard from "../../components/PostCard/PostCard";
+
+import "./singlepost.css";
+import { useQuery } from "@apollo/client";
+import { QUERY_POSTS } from "../../utils/queries";
+
+export default function SinglePost()  {
+  return (
+    <div>SinglePost</div>
+  )
+}
+
